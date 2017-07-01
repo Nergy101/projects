4e5e4a62f146fdee36f0831545f58691d76f75d5
Montarion <jamirograntsaan@gmail.com>
2017-07-01 19:46:53 +0200

    woop woop! client and server udp are working!

--- home-automation/hermes/client.py
+++ home-automation/hermes/client.py
@@ -1,72 +1,75 @@
 from time import *
 from socket import *
 import random, string
-import threading
+
 import os
-global localstatus, statuscode, addr
-localstatus = '0000'
-statuscode = '0000'
-addr = '0000'
+global localstatus, addr, statuscode
+localstatus = '0000'            #CODE
+addr = '0000'                   #CODE
+statuscode = '0000'             #CODE meaning go
 #name = input('name?')
 name = 'hawk'
 base = name + ":" + "CLIENT" + ":"
 def receive():
-    print('recieving')
+    global statuscode
 
-    host = ""
-    port = 13000
-    buf = 1024
-    addr = ((host, port))
-    UDPSock = socket(AF_INET, SOCK_DGRAM)
-    UDPSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    UDPSock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-    UDPSock.bind(addr)
+    print('discovery mode activated, waiting for data')
+
 
     while True:
-        print('discovery mode activated, waiting for data')
+
+        host = ""
+        port = 13000
+        buf = 1024
+        addr = ((host, port))
+        UDPSock = socket(AF_INET, SOCK_DGRAM)
+        UDPSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        UDPSock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        UDPSock.bind(addr)
+        #print('waiting')
         (data, addr) = UDPSock.recvfrom(buf)
+        #print('got the deets')
+        UDPSock.close()
         data = str(data)
         addr = str(addr)
-        print(data)
+        ip = addr[2:addr.find(',')-1]
+        #print(data)
 
         #--commands--#
-        if statuscode == '9999':
-            break
-        if data[-5:-1] == '1000':
+        if data[-5:-1] == '1000':       #CODE  meaning discover
             print('probe recieved, sending reply.')
-            localstatus = '0001'
-            print(localstatus)
-            print('server ip adress is :', addr)
+            localstatus = '0001'        #CODE  meaning disack
+            #print(localstatus)
+            #print('server ip adress is :', addr)
             msg = base + localstatus
             sending(msg)
 
-            UDPSock.close()
+
+        if data[-5:-1] == '0002':       #CODE  meaning udp connected
+            localstatus = '0002'
+            print('preliminary connection established, setting up secure connection to', ip)
             break
-        sleep(3)
+    return ip
 
 def sending(msg):
+    global localstatus
+    if localstatus == '0002':
+        os._exit()
     dest = ('<broadcast>')
     port = 13000
     addr = (dest, port)
     BSock = socket(AF_INET, SOCK_DGRAM)
     BSock.setsockopt(SOL_SOCKET,SO_BROADCAST, 1)
-    while True:
-        BSock.sendto(bytes(msg, "utf-8"), addr)
-        print('sending reply')
-        sleep(2)
-    statuscode = '9999'
-    print('done, status code is' + statuscode)
+    sleep(3)
+    BSock.sendto(bytes(msg, "utf-8"), addr)
+    #print('sending reply')
+    sleep(2)
+    localstatus = '0000'
 
     BSock.close()
-    #os._exit(0)
 
 
-threads = []
 
-t1 = threading.Thread(target=receive)
+receive()
 
-#t2 = threading.Thread(target=checking)
-#t2.start()
-sleep(2)
-t1.start()
 
